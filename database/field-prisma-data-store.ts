5843beefb8a28beb82d2ea3d38e96580f12fb610
amashamaduwanthi <amashamaduwanthi123@gmail.com>
2025-01-31 11:21:51 +0400

    field delete update getall successfully

--- database/field-prisma-data-store.ts
+++ database/field-prisma-data-store.ts
@@ -1,5 +1,6 @@
 import {PrismaClient} from "@prisma/client";
 import Field from "../model/Field";
+import exp from "constants";
 
 const prisma = new PrismaClient();
 
@@ -21,5 +22,42 @@ export async function FieldAdd(f:Field){
         return newField;
     }catch (err){
         console.log("Field Add unsuccessfully",err)
+    }
+
+}
+export async function getAllFields(){
+    try {
+        return await prisma.field.findMany();
+    }catch (err){
+        console.log("error getting field data",err)
+    }
+}
+export async function DeleteField(fieldCode:string){
+    try{
+        const deleteField=await prisma.field.delete({
+            where:{fieldCode:fieldCode}
+        })
+        console.log("Field delete  Successfully")
+        return deleteField
+    }catch (err){
+        console.log("Field delete Unsuccessfully",err)
+
+    }
+}
+export async function UpdateField(fieldCode:string,f:Field){
+    try {
+        const updateField=await prisma.field.update({
+            where:{fieldCode:fieldCode},
+            data:{
+                fieldName:f.fieldName,
+                location:f.location,
+                extendSize:f.extendSize,
+                fieldImage:f.fieldImage
+            }
+        })
+        console.log("Field update successfully")
+        return updateField
+    }catch (err){
+        console.log("Filed Update unsuccessfully",err)
     }
 }
--- routes/field-routes.ts
+++ routes/field-routes.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import Field from "../model/Field";
-import {FieldAdd} from "../database/field-prisma-data-store";
+import {DeleteField, FieldAdd, getAllFields, UpdateField} from "../database/field-prisma-data-store";
 
 const router = express.Router();
 router.post('/add',async (req,res)=>{
@@ -13,5 +13,35 @@ router.post('/add',async (req,res)=>{
         console.log("Error Adding Fields",err);
         res.send("Error Adding Field").status(400);
     }
+})
+router.get('/view',async (req,res)=>{
+    try {
+        const fields=await getAllFields();
+        res.json(fields);
+    }catch (err){
+        console.log("error getting fields",err)
+    }
+
+})
+router.delete('/delete/:fieldCode',async (req,res)=>{
+    const fieldCode: string  = req.params.fieldCode;
+    try{
+        const deleteField=await DeleteField(fieldCode);
+        res.json(deleteField)
+    }catch (err){
+        console.log("Error deleting Field",err)
+    }
+})
+router.put('/update/:fieldCode',async (req,res)=>{
+    const field:Field=req.body;
+    const fieldCode: string  = req.params.fieldCode;
+    try {
+        const updateField=await UpdateField(fieldCode,field);
+        res.send(updateField)
+    }catch (err){
+        console.log("Error updating Field",err)
+        res.status(400)
+    }
+
 })
 export default router
