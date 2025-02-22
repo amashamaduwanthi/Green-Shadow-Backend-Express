f6128e28ff0e46526f83d1dbeb065dd6c02fe2b9
amashamaduwanthi <amashamaduwanthi123@gmail.com>
2025-02-22 10:25:05 +0400

    authentication successfully

--- server.ts
+++ server.ts
@@ -5,22 +5,26 @@ import staffRoutes from "./routes/staff-routes";
 import vehicleRoutes from "./routes/vehicle-routes";
 import equipmentRoutes from "./routes/equipment-routes";
 import logRoutes from "./routes/log-routes";
+import authRoutes, {authenticateToken} from "./routes/authRoutes";
+import dotenv from "dotenv";
+import cors from 'cors';
 const app = express();
+dotenv.config();
 
 app.use(express.json())
-app.use('/',(req,res,next)=>{
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, content-type');
-
-    next();
-})
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use('/field',fieldRoutes)
 app.use('/crop',cropRoutes)
 app.use('/staff',staffRoutes)
 app.use('/vehicle',vehicleRoutes)
 app.use('/equipment',equipmentRoutes)
 app.use('/log',logRoutes);
+app.use('/auth', authRoutes);
 
 app.listen(3005, (err=>{
     console.log("Server running on port 3002");
@@ -29,4 +33,8 @@ app.listen(3005, (err=>{
 app.use('/',(req,res,next)=>{
     res.status(200).send('Not Found');
 })
+
+
+console.log("Loaded SECRET_KEY:", process.env.SECRET_KEY);
+app.use(authenticateToken);
 
